854a9fc94e45e71d907a08dfc3ea85aa0922062e
Alvin <darkwell463@gmail.com>
2023-10-27 15:24:17 +0300

    10

--- tasks/part_1_beginner/task_10.py
+++ tasks/part_1_beginner/task_10.py
@@ -1,1 +1,19 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+ALPHABETS: dict = {
+    "vowels": ("aeiou"
+                  "AEIOU"
+                  "аеиёоуыэюя"
+                  "АЕЁИОУЫЭЮЯ"),
+    "consonants": ("bdcdfghjklmnpqrstvwxyz"
+                   "BCDFGHJKLMNPQRSTVWXYZ"
+                   "бвгджзйклмнпрстцфчьжхъщш"
+                   "БВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ"),
+}
+word: str = input()
+set_vowels = ""
+set_consonants = ""
+for item in word:
+    if item in ALPHABETS["vowels"]:
+        set_vowels += item
+    if item in ALPHABETS["consonants"]:
+        set_consonants += item
+print(len(set_vowels), len(set_consonants), sep="\n")
