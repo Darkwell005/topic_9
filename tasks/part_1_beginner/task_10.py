f880a4df23e60640c62e23f1b7083095b2ff8c2e
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-10-29 08:29:54 +0000

    Проверка ДЗ | 9. Работа со строками [Начинающий]
    - Правильно: №10
    - Неправильно:
    - Улучшены:
    - На доработку: №10

--- tasks/part_1_beginner/task_10.py
+++ tasks/part_1_beginner/task_10.py
@@ -1,19 +1,25 @@
 ALPHABETS: dict = {
-    "vowels": ("aeiou"
-                  "AEIOU"
-                  "аеиёоуыэюя"
-                  "АЕЁИОУЫЭЮЯ"),
-    "consonants": ("bdcdfghjklmnpqrstvwxyz"
-                   "BCDFGHJKLMNPQRSTVWXYZ"
-                   "бвгджзйклмнпрстцфчьжхъщш"
-                   "БВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ"),
+    "vowels": ("aeiouAEIOU"
+               "аеиёоуыэюяАЕЁИОУЫЭЮЯ"),
+    "consonants": ("bdcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+                   "бвгджзйклмнпрстцфчьжхъщшБВГДЖЗЙКЛМНПРСТФХЦЧШЩЪЬ"),
 }
+
 word: str = input()
+
+# 1. Здесь лучше использовать счетчик, вместо создания новой строки
 set_vowels: str = ""
 set_consonants: str = ""
+
 for item in word:
     if item in ALPHABETS["vowels"]:
         set_vowels += item
+
+    # 2. Лишняя проверка if, исправьте пожалуйста на соответствующий оператор
     if item in ALPHABETS["consonants"]:
         set_consonants += item
+
+# 3. Как все исправите, здесь нужно будет вывести только значение счетчиков
 print(len(set_vowels), len(set_consonants), sep="\n")
+
+# В целом хорошая работа! Молодцы!
