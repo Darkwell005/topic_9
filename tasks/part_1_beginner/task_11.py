cfb3dd27f04d4645401af5e524ac5d217a0ff336
Alvin <darkwell463@gmail.com>
2023-11-02 19:11:59 +0300

    11

--- tasks/part_1_beginner/task_11.py
+++ tasks/part_1_beginner/task_11.py
@@ -1,1 +1,15 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+num: str = input()
+is_num: bool = True
+digits: str = "0123456789"
+symbols: str = "+-. "
+dots_count: int = 0
+for item in num:
+    if (item not in digits) and (item not in symbols):
+        is_num = False
+        break
+    elif item == ".":
+        dots_count += 1
+    if dots_count > 1:
+        is_num = False
+        break
+print(is_num)
