09c567b190c4fb3259930e0202fb54f8468f297d
Alvin <darkwell463@gmail.com>
2023-11-12 14:16:42 +0300

    3, 7

--- tasks/part_2_insightful/task_7.py
+++ tasks/part_2_insightful/task_7.py
@@ -1,1 +1,23 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+numbers = "0123456789"
+while True:
+    password = input()
+    if len(password) < 8:
+        print("Пароль должен "
+              "быть минимум 8 символов")
+
+    elif len(password) > 255:
+        print("Пароль не может "
+              "быть больше 255 символов")
+
+    elif password.isalpha():
+        print("Пароль не должен состоять "
+              "только из буквенных символов")
+
+    elif password.isdecimal():
+        print("Пароль не должен состоять только из цифр")
+    else:
+        for item in password:
+            if item not in numbers:
+                break
+            else:
+                print("Пароль не может начинаться с цифры")
