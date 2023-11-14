1aaf277e94ea4feb7cc3497e31675e8568a3c6f0
Alvin <darkwell463@gmail.com>
2023-11-14 15:59:12 +0300

    7

--- tasks/part_2_insightful/task_7.py
+++ tasks/part_2_insightful/task_7.py
@@ -6,11 +6,11 @@ while True:
         print("Пароль должен "
               "быть минимум 8 символов")
 
-    if len(password) > 255:
+    elif len(password) > 255:
         print("Пароль не может "
               "быть больше 255 символов")
 
-    if password.isalpha():
+    elif password.isalpha():
         print("Пароль не должен состоять "
               "только из буквенных символов")
 
@@ -18,5 +18,25 @@ while True:
         print("Пароль не должен состоять только из цифр")
 
     # if password.startswith(tuple(numbers)):
-    if password[0].isdigit():
+    elif password[0].isdigit():
         print("Пароль не может начинаться с цифры")
+
+    elif not password[-1].isalnum():
+        print("Пароль должен заканчиваться буквой или цифрой")
+
+    elif password.isalnum():
+        print("ПРЕДУПРЕЖДЕНИЕ: Ваш пароль состоит только из букв и цифр")
+        confirmation = input("Хотите добавить специальные символы?(y/N):")
+        confirmation = confirmation.lower()
+
+        if confirmation == "n":
+            print("\nПароль принят!")
+            break
+
+        elif confirmation == "y":
+            password = input()
+            print("\nПароль принят!")
+            break
+    else:
+        print("\nПароль принят!")
+        break
