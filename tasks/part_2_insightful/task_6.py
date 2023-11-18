9abc7bc3a30ec79d374049265360ed1db7e8aeec
Alvin <darkwell463@gmail.com>
2023-11-18 11:34:35 +0300

    Решение V9 Домашнее задание | 1. Начало работы
    - Решение задач: 6.

--- tasks/part_2_insightful/task_6.py
+++ tasks/part_2_insightful/task_6.py
@@ -1,1 +1,12 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+line = input()
+count_num = 0
+count_let = 0
+count_space = 0
+for i in line:
+    if i.isnumeric():
+        count_num += 1
+    if i.isalpha():
+        count_let += 1
+    if i.isspace():
+        count_space += 1
+print(count_let, count_num, count_space, sep="\n")
