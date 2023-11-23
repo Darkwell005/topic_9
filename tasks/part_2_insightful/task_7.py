c88e0a663e0e861ee23ca6b7f6f926be1f3d36bd
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-11-23 16:25:00 +0000

    Проверка ДЗ | 9. Работа со строками [Познаватель]
    - Правильно:
    - Неправильно:
    - Улучшены: №7
    - На доработку:

--- tasks/part_2_insightful/task_7.py
+++ tasks/part_2_insightful/task_7.py
@@ -1,37 +1,30 @@
-numbers = "0123456789"
+numbers: str = "0123456789"
 
 while True:
-    password = input()
-    length = len(password)
+    password: str = input()
+    length: int = len(password)
+
     if length < 8:
         print("Пароль должен быть минимум 8 символов")
-
     elif length > 255:
         print("Пароль не может быть больше 255 символов")
-
     elif password.isalpha():
         print("Пароль не должен состоять "
               "только из буквенных символов")
-
     elif password.isdecimal():
         print("Пароль не должен состоять только из цифр")
-
     elif password[0].isdigit():
         print("Пароль не может начинаться с цифры")
-
     elif not password[-1].isalnum():
         print("Пароль должен заканчиваться буквой или цифрой")
-
     elif password.isalnum():
         print("ПРЕДУПРЕЖДЕНИЕ: Ваш пароль состоит только из букв и цифр")
         confirmation = input("Хотите добавить специальные символы?(y/N): ").lower()
 
-        if confirmation == "n":
-            print("\nПароль принят!")
-            break
-
-        elif confirmation == "y":
+        if confirmation == "y":
             continue
+    break
 
-        print("\nПароль принят!")
-        break
+print("\nПароль принят!")
+
+# Валидация - обработка данных на соответствие
