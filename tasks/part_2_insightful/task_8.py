a53d3ce8c477bf8f8ea4c3cfab0e421e9d210e6b
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-11-26 11:28:46 +0000

    Проверка ДЗ | 9. Работа со строками [Познаватель]
    - Правильно:
    - Неправильно:
    - Улучшены:
    - На доработку: №8

--- tasks/part_2_insightful/task_8.py
+++ tasks/part_2_insightful/task_8.py
@@ -1,28 +1,22 @@
 names = input().split()
+
 vowels = "АУЕЫОЭЯИЮауеыоэяию"
+
 max_vowels = -1
-count_vowels = 0
-empty_list = []
+result = []
+
 for name in names:
+    count_vowels = 0
     for letter in name:
         if letter in vowels:
             count_vowels += 1
-    
-    print(name)
-print(empty_list)
-# Счетчик_макс_гласных
 
-# Список_для_хранения_имен_с_максимальным_кол-вом_гласных
-# Счетчик
-# Сначала посчитаем количество гласных в имени
-# Сравнить количество гласных с максимальным
+    if ...:
+        ...
+        # Сбросить и положить в список текущее имя
+    elif ...:
+        # Добавить имя в список, так как длина гласных букв в
+        # этих именах совпадает.
+        ...
 
-
-# nums = [10, 6, 56, 23, 7, 2]
-# mx_num = -1
-#
-# for n in nums:
-#     if n > mx_num:
-#         mx_num = n
-# print(mx_num)
-# print(max(nums))
+print(result)
