e3720377e0be3361c9b7cfe2ff1b1729dcf4910a
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-11-26 07:15:36 +0000

    Проверка ДЗ | 9. Работа со строками [Познаватель]
    - Правильно: №5
    - Неправильно:
    - Улучшены:
    - На доработку:

--- tasks/part_2_insightful/task_5.py
+++ tasks/part_2_insightful/task_5.py
@@ -1,5 +1,8 @@
 user: str = input()
+
 vowels: str = 'aeyuoiAEYIUOауеыоэяиюАУЕЫОЭЯИЮ'
 symbol: str = "?" * len(vowels)
-x = user.maketrans(vowels, symbol)
+
+# Метод maketrans возвращает словарь
+x: dict = user.maketrans(vowels, symbol)
 print(user.translate(x).strip())
