58e634301906390894393cad38f789230e3adc5c
Alvin <darkwell463@gmail.com>
2023-12-03 12:12:44 +0300

    Решение V9 Домашнее задание | 3. Профессионал
    - Решение задач: 0, 1, 2.

--- tasks/part_3_professional/task_0.py
+++ tasks/part_3_professional/task_0.py
@@ -1,1 +1,8 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+word: str = input()
+vowels: str = "АУЕЫОЭЯИЁЮауеыоёэяиюAEUIOaeuio"
+count_vowel: int = 0
+for item in word:
+    if item in vowels:
+        count_vowel += 1
+attitude: float = count_vowel / (len(word)) * 100
+print(f"Отношение гласных: {attitude:05.2f}%")
--- tasks/part_3_professional/task_2.py
+++ tasks/part_3_professional/task_2.py
@@ -1,1 +1,24 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+while True:
+    print('Программа "Конвертер валют"\n')
+
+    print("Выберите операцию (0 для выхода):\n"
+          "1. Конвертировать рубли в доллары\n"
+          "2. Конвертировать доллары в рубли")
+    operation: str = (input("Введите номер операции: "))
+    if not operation.isnumeric():
+        print("Введите числовое значение (номер операции)")
+    elif ((operation != "0")
+          and (operation != "1")
+          and (operation != "2")):
+        print("Неверный выбор операции. Попробуйте ещё раз.")
+    elif operation == "0":
+        print("До свидания!")
+        break
+    elif operation == "1":
+        exchange_rate = float(input("Введите курс доллара к рублю: "))
+        quantity = float(input("Введите количество рублей: "))
+        print(f"Вы получите {exchange_rate * quantity:05.2f} USD\n")
+    elif operation == "2":
+        exchange_rate: float = float(input("Введите курс доллара к рублю: "))
+        quantity: float = float(input("Введите количество рублей: "))
+        print(f"Вы получите {exchange_rate * quantity:05.2f} RUB\n")
