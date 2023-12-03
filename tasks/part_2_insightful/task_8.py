3ddbcbe4637a309a252b335fd3b72e35d6a99321
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-12-03 11:48:25 +0000

    Проверка ДЗ | 9. Работа со строками [Познаватель]
    - Правильно: №8
    - Неправильно:
    - Улучшены: №8, Опеачтка в примере
    - На доработку:

--- tasks/part_2_insightful/task_8.py
+++ tasks/part_2_insightful/task_8.py
@@ -1,20 +1,20 @@
 names: list = input().split()
 
-vowels: str = "АУЕЫОЭЯИЮауеыоэяию"
+vowels: str = "ауеыоэяию"
 
 max_vowels: int = -1
 result: list = []
 
 for name in names:
     count_vowels = 0
-    for letter in name:
+    for letter in name.lower():
         if letter in vowels:
             count_vowels += 1
 
     if max_vowels < count_vowels:
-        result = [name]
+        result.clear()
+        result.append(name)
         max_vowels = count_vowels
-
     elif max_vowels == count_vowels:
         result.append(name)
 
