3114522ac318cd32506929c97e6e324efed7efd8
Alvin <darkwell463@gmail.com>
2023-12-07 18:32:58 +0300

    Решение V9 Домашнее задание | 1. Начало работы
    - Решение задач: 3.

--- tasks/part_3_professional/task_3.py
+++ tasks/part_3_professional/task_3.py
@@ -1,1 +1,24 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+discount = int(input())
+price = input().split()
+
+commodity = "Товар"
+price_hint = "Цена"
+title = "Название"
+discount_amount_hint = "Сумма скидки"
+new_price_hint = "Новая цена"
+
+if (discount <= 0) or (discount > 100):
+    print("Размер скидки должен "
+          "быть больше 0 и не "
+          "должен превышать 100")
+
+print(f"{title:<15} "
+      f"{price_hint:<10} "
+      f"{discount_amount_hint:<15} "
+      f"{new_price_hint:<10}")
+
+for item in price:
+    num = float(item)
+    discount_amount = num * discount / 100
+    print(f"{discount_amount:.2f}")
+
