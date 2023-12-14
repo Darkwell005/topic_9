f747d2e1efce6293980e1136944fb0f7090ec489
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-12-14 16:37:57 +0000

    Проверка ДЗ | 9. Работа со строками [Профессионал]
    - Правильно:
    - Неправильно:
    - Улучшены:
    - На доработку: №5

--- tasks/part_3_professional/task_5.py
+++ tasks/part_3_professional/task_5.py
@@ -1,15 +1,25 @@
-names: list[str, ...] = []
+names: list[str] = []
+
 i = 1
 while True:
     full_name: str = input().strip()
     if full_name.lower() == "end":
         break
-    else:
-        names.append(full_name)
+
+    last_name, first_name, patronymic = full_name.split()
+
+    result = (f'{i:0>2} | {last_name.title()} '
+              f'{first_name[0].upper()}'
+              f'.{patronymic[0].upper()}.')
+
+    names.append(result)
     # Добавлять в список номер строки|, ФИО
 
     i += 1
 
 print(" № ФИО")
 for name in names:
-    ...
+    print(name)
+
+
+format()
