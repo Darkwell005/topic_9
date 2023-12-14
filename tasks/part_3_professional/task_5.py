de30ef10336cf65c3dd89acf62353906daa5cd18
Alvin <darkwell463@gmail.com>
2023-12-14 18:44:03 +0300

    Решение V9 Домашнее задание | 1. Начало работы
    - Решение задач: 5.

--- tasks/part_3_professional/task_5.py
+++ tasks/part_3_professional/task_5.py
@@ -1,1 +1,15 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+names: list[str, ...] = []
+i = 1
+while True:
+    full_name: str = input().strip()
+    if full_name.lower() == "end":
+        break
+    else:
+        names.append(full_name)
+    # Добавлять в список номер строки|, ФИО
+
+    i += 1
+
+print(" № ФИО")
+for name in names:
+    ...
