b51eea72eef27d1c23631c4e3434f55895a73e6b
Alvin <darkwell463@gmail.com>
2023-12-17 14:23:05 +0300

    Решение V9 Домашнее задание | 9. Работа со строками [Мастер]
    - Решение задач: 0.

--- tasks/part_4_master/task_0.py
+++ tasks/part_4_master/task_0.py
@@ -1,1 +1,11 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+words: list = input().split()
+f = ""
+for word in words:
+    if len(word) >= 5:
+        print(word[::-1], end=" ")
+words = input().split()
+f = ""
+for word in words:
+    if len(word) >= 5:
+        f += word[::-1]
+print(f)
