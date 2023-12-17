8c74c11d6488563885e2c79c2572a66ba2ec4e00
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-12-17 11:22:16 +0000

    Проверка ДЗ | 9. Работа со строками [Профессионал]
    - Правильно:
    - Неправильно: №5,
    - Улучшены: №5
    - На доработку:

--- tasks/part_3_professional/task_5.py
+++ tasks/part_3_professional/task_5.py
@@ -1,25 +1,32 @@
 names: list[str] = []
-max_name = 0
-i = 1
+last_name_max_len: int = 0
+i: int = 1
 while True:
     full_name: str = input().strip()
     if full_name.lower() == "end":
         break
 
     last_name, first_name, patronymic = full_name.split()
+    length: int = len(last_name)
+    if length > last_name_max_len:
+        last_name_max_len = length
 
-    result = (f'{i:0>2} | {last_name.title()} '
-              f'{first_name[0].upper()}'
-              f'.{patronymic[0].upper()}.')
-
+    result: str = (f'{i:0>2} | {last_name.title()} '
+                   f'{first_name[0].upper()}'
+                   f'.{patronymic[0].upper()}.')
     names.append(result)
-    # Добавлять в список номер строки|, ФИО
-
     i += 1
-    if len(last_name) > 0:
-        max_name = len(last_name)
-        
-hint = "ФИО"
-print(f"{hint:^{max_name}}")
+
+table_num: str = "№"
+table_fio: str = "ФИО"
+print(f"{table_num:^3}{table_fio:^{last_name_max_len}}")
+
 for name in names:
     print(name)
+
+# Option 2
+# print(*names, sep='\n')
+
+# Option 3
+# res: str = "\n".join(names)
+# print(res)
