a2793de8d2ed64fd67bb9b19fa5729018337426b
Alvin <darkwell463@gmail.com>
2023-12-22 20:27:58 +0300

    Решение V9 Домашнее задание | 9. Работа со строками [Мастер]
    - Решение задач: 2.

--- tasks/part_4_master/task_2.py
+++ tasks/part_4_master/task_2.py
@@ -1,1 +1,14 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+# Например: 77.88.21.8.
+# 77 * 256^3 + 88 * 256^2 +
+# 21 * 256^1 + 8 * 256^0 =
+# 1297618184.
+line: list = input().split(".")
+num: int = 256
+degree: int = 3
+
+result: int = 0
+for item in line:
+    item = int(item)
+    result += item * (num ** degree)
+    degree -= 1
+print(result)
