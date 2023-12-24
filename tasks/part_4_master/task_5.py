270f5b078da0f0f5e6f3bd98ef9a3161f1dafd93
Alvin <darkwell463@gmail.com>
2023-12-24 13:24:18 +0300

    Решение V9 Домашнее задание | 9. Работа со строками [Мастер]
    - Решение задач: 5.

--- tasks/part_4_master/task_5.py
+++ tasks/part_4_master/task_5.py
@@ -1,1 +1,9 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+line = input().strip()
+flag = True
+for i in line:
+    if i.isupper() and flag:
+        flag = False
+    elif i.isupper():
+        line = line.replace(i, "_" + i.lower())
+
+print(line.lower())
